fe612e9c389ed3e6696c55a7a59613027cf5fa74
Khagan95 <85906243+Khagan95@users.noreply.github.com>
2023-08-23 18:02:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UcgenCizimUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgenin boyutu: ");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+            UcgenCizici ucgenCizici = new UcgenCizici();
+            ucgenCizici.UcgenCiz(boyut);
+        }
+    }
+
+    class UcgenCizici
+    {
+        public void UcgenCiz(int boyut)
+        {
+            for (int i = 1; i <= boyut; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
